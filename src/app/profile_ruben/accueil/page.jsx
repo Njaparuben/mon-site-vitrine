6021967188ebdd1f7eb6504ab20dc339f76718e7
Njaparuben <rubennjapa@gmail.com>
2025-06-18 03:43:44 +0200

    Premier commit : initialisation du projet 18-06-2025

---
+++ src/app/profile_ruben/accueil/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default async function Acceuil(){
+    return(
+        <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+            <h1 className="text-4xl font-bold mb-4">Bienvenue sur mon site vitrine</h1>
+            <p className="text-lg text-gray-700 mb-8">Je suis Ruben, un développeur passionné.</p>
+            <a href="/profile_ruben/portfolio" className="px-6 py-3 bg-blue-600 text-white  rounded hover:bg-blue-700 transition duration-300 ">
+                Voir mon portfolio
+            </a>
+        </div>
+    )
+}
