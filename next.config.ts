c63e6e57a02ae820e3493c623314efcd22f69236
Njaparuben <rubennjapa@gmail.com>
2025-06-21 19:58:00 +0200

    commit ruben 21-06-2025

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Cette ligne est cruciale pour l'exportation statique
+  images: {
+    unoptimized: true, // Recommandé pour GitHub Pages car il ne supporte pas l'optimisation d'images de Next.js
+  },
+  // Si votre site GitHub Pages est hébergé dans un sous-répertoire (ex: username.github.io/repo-name)
+  // alors vous devrez aussi configurer basePath et assetPrefix
+  // basePath: '/nom-de-votre-repo',
+  // assetPrefix: '/nom-de-votre-repo/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+export default nextConfig as NextConfig;
